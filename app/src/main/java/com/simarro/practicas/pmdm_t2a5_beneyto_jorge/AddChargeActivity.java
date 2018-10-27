2c05037781148e3b96f2d296bae7e596c636219f
jorbencas <jorbencas@gmail.com>
2018-10-27 17:56:10 +0200

    Finish funciontality of transation action

--- app/src/main/java/com/simarro/practicas/pmdm_t2a5_beneyto_jorge/AddChargeActivity.java
+++ app/src/main/java/com/simarro/practicas/pmdm_t2a5_beneyto_jorge/AddChargeActivity.java
@@ -7,37 +7,35 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class AddChargeActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class AddChargeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
     private GridView grdOpciones;
-    private ArrayAdapter<String> adapatador;
+    private ArrayAdapter<String> adapatador, adapatadordivisa;
     String[] datos = new String[]{"XXXX XXXX XX XXXXXXXXXX","XXXX XXXX XX XXXXXXXXXX","XXXX XXXX XX XXXXXXXXXX","XXXX XXXX XX XXXXXXXXXX"};
-    private RadioButton transfersomeoneAccount;
-    private RadioButton transfermyAccount;
+    private RadioButton transfersomeoneAccount, transfermyAccount;
     private EditText setAccount;
     private Spinner cmdopciones;
-    private ArrayAdapter<String> adapatadordivisa;
     String[] divisa = new String[]{"€", "$"};
-    private Button btncancel;
-    private Button btnok;
+    private CheckBox justifyed;
+    private Button btncancel, btnok;
+    Charge charge = new Charge();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_charge);
 
-        grdOpciones = (GridView) findViewById(R.id.GridOpciones);
-
-
         grdOpciones = (GridView)findViewById(R.id.GridOpciones);
         adapatador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datos);
         grdOpciones.setAdapter(adapatador);
+        grdOpciones.setOnItemClickListener(this);
 
         transfersomeoneAccount = (RadioButton)findViewById(R.id.transfersomeoneAccount);
         transfermyAccount = (RadioButton)findViewById(R.id.transfermyAccount);
@@ -47,6 +45,9 @@ public class AddChargeActivity extends AppCompatActivity implements View.OnClick
         adapatadordivisa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, divisa);
         adapatadordivisa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cmdopciones.setAdapter(adapatadordivisa);
+        cmdopciones.setOnItemSelectedListener(this);
+
+        justifyed = (CheckBox)findViewById(R.id.justifyed);
 
         btnok = (Button)findViewById(R.id.btnok);
         btncancel = (Button)findViewById(R.id.btncancel);
@@ -55,22 +56,29 @@ public class AddChargeActivity extends AppCompatActivity implements View.OnClick
         btncancel.setOnClickListener(this);
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnok:
-                Toast.makeText(this, "Pulsado boton de añadir", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(AddChargeActivity.this, GridActivity.class));
+                charge.setAccount(setAccount.getText().toString());
+                Toast.makeText(this, charge.toString(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btncancel:
+                reloadactivity();
                 Toast.makeText(this, "Pulsado boton de añadir", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    public void reloadactivity(){
+        new Charge();
+        adapatador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datos);
+        grdOpciones.setAdapter(adapatador);
+        transfermyAccount.setChecked(Boolean.FALSE);
+        transfersomeoneAccount.setChecked(Boolean.FALSE);
+        setAccount.setVisibility(View.INVISIBLE);
+        cmdopciones.setSelection(0);
+        justifyed.setChecked(Boolean.FALSE);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -86,8 +94,31 @@ public class AddChargeActivity extends AppCompatActivity implements View.OnClick
             case R.id.transfermyAccount:
                 if (checked)
                     setAccount.setVisibility(View.INVISIBLE);
+                    charge.setAccount("12345678920147852369");
                 break;
         }
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        charge.setNumber_account(String.valueOf(parent.getItemAtPosition(position)));
+    }
+
+    public void sendjustified(View view){
+        if (justifyed.isChecked()){
+            charge.setJusfityer(Boolean.TRUE);
+        }else{
+            charge.setJusfityer(Boolean.FALSE);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        char divise = (char) String.valueOf(parent.getItemAtPosition(position)).charAt(0);
+        charge.setDivise(divise);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { charge.setDivise(' '); }
 }
